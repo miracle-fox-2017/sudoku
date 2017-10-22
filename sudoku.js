d5a51d1b024b770422aba8c2f2329934f3040b61
Ayunda <ayundahamzah@gmail.com>
2017-10-22 10:42:36 +0700

    release 4

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,114 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string=board_string.split('');
+    this.irisan=[];
+    this.arrZero=[];
+    this.makeResult=[];
+  }
 
-  solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let setPanjang = this.board_string.length / 9;
+    let counter = 0;
+    for (let i = 0; i < setPanjang; i++) {
+      this.irisan.push([]);
+      for (let j = 0; j < setPanjang; j++) {
+        this.irisan[i].push(this.board_string[counter]);
+        counter++;
+      }
+    }
+    return this.irisan;
+  }
+
+
+  //method findZero mencari nilai 0
+  findZero (){
+    this.board();
+    // console.log(this.board());
+    for (let i = 0; i < this.irisan.length; i++) {
+      for (let j = 0; j < this.irisan.length; j++) {
+        if (this.irisan[i][j] == 0) {
+          let zeroCoor = i+''+j;
+          this.arrZero.push(zeroCoor);
+        }
+      }
+    }
+    console.log(this.arrZero)
+    return this.arrZero;
+  }
+    //method rowCheck untuk mengecek nilai per BARIS
+  rowCheck(baris, check) {
+    let getRow = baris[1];
+    for (let i = 0; i < 9; i++) {
+      if (this.irisan[i][getRow] == check) {
+        return true;
+      }
+    }
+    return false;
+  }
+  //method colCheck ntuk mengecek nilai per KOLOM
+  colCheck(kolom,check){
+    let getCol = kolom[0];
+    for(let i = 0; i < 9;i++){
+      if (this.irisan[getCol][i]==check){
+        return true;
+      }
+    }
+    return false;
+    }
+  //method checkArea untuk mengecek area 3x3 di board
+  checkArea(baris, check) {
+    let getCol = Math.floor(baris[1] / 3) * 3;
+    let getRow = Math.floor(baris[0] / 3) * 3;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+          if (this.irisan[getRow+i][getCol+j] == check) {
+            return true;
+          }
+        }
+      }
+  return false;
+}
+//method untuk mengganti nilai 0 dengan result dengan memanggil 3 method checker
+ solve() {
+  this.findZero();
+  for (let i = 0; i < this.arrZero.length; i++) {
+    let check = 1;
+    let set = true;
+    for (let j = 1; j <= 9; j++) {
+      if (this.colCheck(this.arrZero[i], check) === false) {
+      if (this.rowCheck(this.arrZero[i], check) === false) {
+      if (this.checkArea(this.arrZero[i], check) === false) {
+            this.irisan[this.arrZero[i][0]][this.arrZero[i][1]] = String(check);
+            set = false;
+            }
+          }
+        }
+        check++;
+      }
+    }
+    return this.irisan;
+  }
+  //method yang akan mencetak board hasil
+  resultBoard() {
+    this.solve();
+    let rowBoard = this.irisan.length;
+    let colBoard = this.irisan[0].length / 3;
+    for (let i = 0; i < rowBoard; i++){
+      this.makeResult.push([]);
+      for (let j = 0; j < colBoard; j++){
+        this.makeResult[i].push([]);
+        for (let k = j*3; k < 3*(j+1);k++){
+          this.makeResult[i][j].push(this.irisan[i][k])
+        }
+      }
+    }
+    return this.makeResult
+  }
+
 }
 
 // The file has newlines at the end of each line,
@@ -17,8 +119,9 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .split("\n")[0]
 
 var game = new Sudoku(board_string)
+// var game = new Sudoku(‘105802000090076405200400819019007306762083090000061050007600030430020501600308900’);
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
-console.log(game.board())
+console.log(game.resultBoard())
