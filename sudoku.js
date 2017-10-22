cb17bfe0c64f0775dd3c735066c423fa5a9f0ec1
ferdy26790 <ferdy26790@gmail.com>
2017-10-22 09:39:53 +0700

    masih rusak sudokunya

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,81 @@
 "use strict"
 
-class Sudoku {
-  constructor(board_string) {}
+ class Sudoku {
+   constructor(board_string) {
+     this.board_string = board_string;
+     this.zeroPos = [];
+     this.board = []
 
+  }
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  print_board() {
+    //membuat papan sudoku dan assign inputannya
+    let counter = 0
+    for(let i = 0 ; i < 9; i++){
+      this.board.push([])
+      for(let j = 0; j < 9; j++){
+        this.board[i].push(this.board_string[counter])
+        counter++
+      }
+    }
+    //mengumpulkan index angka 0
+    for(let i = 0; i < this.board.length;i++){
+      for(let j = 0; j < this.board[i].length;j++){
+        if(this.board[i][j]==0){
+          this.zeroPos.push(i+''+j)
+        }
+      }
+
+    }
+
+
+  }
+  //nomor random untuk input posisi 0
+  randomNum(){
+    return Math.floor(Math.random()*9+1)
+  }
+
+  //fungsi untuk cek ke samping
+    angkaBawah(){
+      //kumpulkan nilai bawah
+      let nilaibawah = []
+      for(let i = 0; i < this.board.length; i++){
+        nilaibawah.push(this.board[i][+this.zeroPos[0][1]])
+
+      }
+      return nilaibawah
+    }
+
+  //masukan angka 0 dan cek
+  assignZero(){
+
+    debugger
+    while(this.zeroPos.length > 0){
+      debugger
+      //simpan nomor random
+      let a = this.randomNum().toString()
+
+
+      //console.log(nilaibawah)
+      //console.log(this.board[+this.zeroPos[0][0]])
+      if(this.board[+this.zeroPos[0][0]].indexOf(a)===-1 ){
+        debugger
+        let b = this.angkaBawah()
+        if(b.indexOf(a) === -1){
+          this.board[+this.zeroPos[0][0]][+this.zeroPos[0][1]]=a
+          this.zeroPos.shift()
+          //b.shift()
+          //console.log(b)
+        }
+
+      }
+
+    }
+
+    console.log(this.board)
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -19,6 +88,12 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
-
-console.log(game.board())
+//game.solve()
+//console.log(game.board())
+game.print_board()
+//console.log(game.board)
+//console.log(game.assignZero())
+//console.log(game.checkSamping())
+//console.log(game.board)
+console.log(game.assignZero())
+// console.log(Sudoku(board_string))
