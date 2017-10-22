b60cc1b8ee5c8d73a444379a27e11b8d958de571
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-22 10:18:15 +0700

    Sudoku

--- sudoku.js
+++ sudoku.js
@@ -8,6 +8,61 @@ class Sudoku {
       this.boardHorizontal=[];
       this.angka=board_string;
     }
+    // Create
+    createHorizontal(){
+        let tempNum=this.angka.split("");
+        for(let i=0;i < this.size;i++){
+            let tempArr=[];
+            for(let j=0;j < this.size;j++){
+                tempArr.push(parseInt(tempNum[0]));
+                tempNum.shift();
+            }
+            this.boardHorizontal.push(tempArr);
+        }
+        return this.boardHorizontal;
+    }
+    createVertical(){
+        let tempNum=this.angka.split("");
+        for(let i=0;i < this.size;i++){
+            let counter=i;
+            let tempArr=[];
+            for(let j=0;j < this.size;j++){
+                tempArr.push(parseInt(tempNum[counter]));
+                counter+=9;
+            }
+            this.boardVertical.push(tempArr);
+        }
+        return this.boardVertical;
+    }
+    createBox(){
+        let tempStart=0;
+        let countSplit=0;
+        let tempNum=this.angka.split("");
+        for(let i=0;i < this.size;i++){
+            let counter=0;
+            let start=i * 3;
+            let tempArr=[];
+            if(i > 2 && i % 3 === 0){
+                start*=3;
+                tempStart=start;
+            }else if(i > 2){
+                countSplit++;
+                start=3 * countSplit + tempStart;
+                countSplit === 2 ? countSplit=0 : countSplit=countSplit;
+            }
+            for(let j=0;j < this.size;j++){
+                if(counter === 3){
+                    start+=6;
+                    counter=0;
+                }
+                counter++;
+                tempArr.push(parseInt(tempNum[start]));
+                start++;
+            }
+            this.boardBox.push(tempArr);
+        }
+        return this.boardBox;
+    }
     solve(){}
     // Returns a string representing the current state of the board
     board(){}
